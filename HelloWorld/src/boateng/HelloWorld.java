b3dbccc51a6c4b5e1d8b8f8e9dcf149a1b802a8e
abigail-boateng <324213115@C-3ZZHYR1.tdsb.on.ca>
2017-02-21 11:25:05 -0500

    This program prints hello World to the console

---
+++ HelloWorld/src/boateng/HelloWorld.java
@@ -0,0 +1,16 @@
+package boateng;
+/**
+ * HelloWold.java
+ * This file prints Hello, World! to the console.
+ * February 21, 2017
+ * @author Abigail Boateng
+ *
+ */
+public class HelloWorld {
+
+	public static void main(String[] args){
+		System.out.println("Hello, World!");
+
+	}
+
+}
